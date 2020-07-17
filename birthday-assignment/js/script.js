0eeff6c83410057db73aa28e8978c18a770d6a8b
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-07-17 16:59:37 +0300

    add the css files and link the js script

---
+++ birthday-assignment/js/script.js
@@ -0,0 +1,5 @@
+maleNames= ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+femaleNames= ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+userGender = ['M', 'F'];
+
+alert(userGender[0])
